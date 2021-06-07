5accd44a9714fb80fa4117a6b36f06a7bad80c6d
kazuki-mura <k.mura0131@gmail.com>
2021-06-07 12:26:43 +0900

    userコントローラーの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'prototypes/index'
+  # get 'prototypes/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
   root to: "prototypes#index"
 end
