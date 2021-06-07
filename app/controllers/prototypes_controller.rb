9f0ffdebb7c20402c3bc514b07d9044d5f8e064d
kazuki-mura <k.mura0131@gmail.com>
2021-06-07 14:51:30 +0900

    Active Storageの導入

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,11 @@
 class PrototypesController < ApplicationController
   def index
   end
-  
+
+
+  private
+
+  def prototype_params
+    params.require(:Prototype).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
