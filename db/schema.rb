44202f7bdaca763befaeb78d4cef7b21fa9b08d9
kazuki-mura <k.mura0131@gmail.com>
2021-06-07 14:43:32 +0900

    テーブルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,10 @@ ActiveRecord::Schema.define(version: 2021_06_07_051649) do
     t.string "title"
     t.text "catch_copy"
     t.text "concept"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_06_07_051649) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "prototypes", "users"
 end
