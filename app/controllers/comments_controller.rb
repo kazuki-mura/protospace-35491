cab2de24358ad77636693c8aca8d32a6fb311eec
kazuki-mura <k.mura0131@gmail.com>
2021-06-08 21:12:19 +0900

    コメント投稿機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,16 @@
 class CommentsController < ApplicationController
   
   def create
+    comment = Comment.create(comment_params)
+    if Comment.create(comment_params)
+      redirect_to "/prototypes/#{comment.prototype.id}"
+    else
+      render prototype_path, method: :get
+    end
   end
 
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
 end
