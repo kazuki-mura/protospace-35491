c5553436f6d065145a43b446b6f2e59a4cd6b790
kazuki-mura <k.mura0131@gmail.com>
2021-06-09 23:19:35 +0900

    チェック時の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  
+
   def create
     #  Comment.create(comment_params)
     if comment = Comment.create(comment_params)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @prototype = Prototype.includes(:user)
+
+    @prototype = @user.prototypes.includes(:user)
   end
 end
